eca3e9c79130f8d4f126ba1f6ac0afe27056788f
ngoanhquoc <73809614+ngoanhquoc@users.noreply.github.com>
2020-11-17 14:48:00 +0700

    them file dang nhap va dang ky

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -1,3 +1,13 @@
 <?php
-require_once "views/layout.php";
+$act = "index";
+if(isset($_GET['act'])==true) $act = $_GET['act'];
+switch ($act) {
+    case 'index':
+        require_once 'views/layout.php';
+        break;
+          case 'danhnhap':
+          require_once 'views/login.php';
+          break;
+}
+
 ?>
