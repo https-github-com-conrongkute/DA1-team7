87c35f9cc3302e5d39adf1e3f8cbbfc38f013466
hailong289 <73808974+hailong289@users.noreply.github.com>
2020-11-21 20:15:11 +0700

    sua loi css

--- site/views/about.php
+++ site/views/about.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
-    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="./views/css/about.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
