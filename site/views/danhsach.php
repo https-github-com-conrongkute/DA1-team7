4362bf7c459206dd3c07de289f90b22c19df680d
conrongkute <64826196+conrongkute@users.noreply.github.com>
2020-11-22 12:45:28 +0700

    datto

--- site/views/danhsach.php
+++ site/views/danhsach.php
@@ -1409,7 +1409,7 @@
         <!-- /.modal -->
 
         <!-- Start of Tawk.to Script-->
-        <script type=text/javascript">
+        <script type=text/javascript>
             var Tawk_API = Tawk_API || {},
                 Tawk_LoadStart = new Date();
             (function() {
