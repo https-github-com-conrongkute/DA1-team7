e65b69a70f786d9e0d443ad1adb9f2b1af59b67f
ngoanhquoc <73809614+ngoanhquoc@users.noreply.github.com>
2020-11-30 23:09:54 +0700

    check lỗi form đăng tin

--- site/views/dangtin.php
+++ site/views/dangtin.php
@@ -10,8 +10,11 @@
     <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-sweetalert/1.0.1/sweetalert.min.js"> </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-sweetalert/1.0.1/sweetalert.min.css">
     <title>Đăng tin</title>
 </head>
 
@@ -198,7 +201,7 @@
         </div>
         <div class="dangtin">
             <h3>ĐĂNG TIN CĂN HỘ</h3>
-            <form action="?ctrl=home&act=dangtincanho&ma_tk=<?=$_SESSION["id"]?>" class="formdangtin" method="POST">
+            <form onsubmit="return validateform()" name="myform" action="?ctrl=home&act=dangtincanho&ma_tk=<?=$_SESSION["id"]?>" class="formdangtin" method="POST">
                 <div class="row thongtincan">
                     <h4 class="w-75 ml-5">* Thông tin căn hộ</h4>
                     <div class="col-6 ml-5">
@@ -210,7 +213,7 @@
                             </div>
                             <div class="col-12" style="margin: 20px 0px;">
                                 <label for="">Tiêu đề cho thuê</label><br>
-                                <input type="text" name="ten_can_ho" id="" class="form-control w-75">
+                                <input type="text" name="ten_can_ho" id="ten" class="form-control w-75">
                             </div>
 
                             <div class="col-6">
@@ -246,7 +249,7 @@
                             
                             <?php if (isset($_SESSION["id"])) {
                                 ?>
-                                <button>Gửi thông tin</button>
+                                <button type="submit">Đăng tin</button>
                                 <?php
                             }
                             else
@@ -304,7 +307,7 @@
                                     <div class="col-6" style="margin: 20px 0px;">
                                         <label for="">Hướng nhà</label>
                                         <select name="huong_nha" class="form-control" id="">
-                                            
+                                            <option value="">Chọn hướng</option>
                                             <option value="1">Đông</option>
                                             <option value="2">Tây</option>
                                             <option value="3">Nam</option>
@@ -434,7 +437,7 @@
                         </div>
                     </div>
                 </div>
-                
+
             </form>
         </div>
         <br>
@@ -534,4 +537,93 @@
                 });
         });
     });
+</script>
+
+<script>
+    function validateform() {
+        var ten = document.myform.ten_can_ho.value;
+        var gia= document.myform.gia_thue.value;
+        var dien_tich= document.myform.dien_tich.value;
+        var tang= document.myform.tang.value;
+        var tien_ich= document.myform.tien_ich.value;
+        var ma_loai= document.myform.ma_loai.value;
+        var ma_quan= document.myform.ma_quan.value;
+
+        var phuong= document.myform.phuong.value;
+        var dia_chi= document.myform.dia_chi.value;
+        var so_phong_ngu= document.myform.so_phong_ngu.value;
+        var so_phong_vs= document.myform.so_phong_vs.value;
+        var hinh= document.myform.hinh.value;
+        var hinha= document.myform.hinha.value;
+        var hinhb= document.myform.hinhb.value;
+        var hinhc= document.myform.hinhc.value;
+ 
+        if (ten == null || ten == "" || ten.length < 10) {
+            swal("Tiêu đề căn hộ không hợp lệ", "Vui lòng đặt lại !", "warning");
+           
+            return false;
+        } else if (Number(gia) < 100000) {
+            swal("Giá thuê không hợp lệ", "Vui lòng đặt lại !", "warning");
+          
+            return false;
+        }
+        else if (Number(dien_tich) < 1) {
+            swal("Diện tích không hợp lệ", "Vui lòng đặt lại !", "warning");
+            return false;
+        }
+        else if (Number(tang) < 0 || tang=="") {
+            swal("Số tầng không hợp lệ", "Vui lòng đặt lại !", "warning");
+            return false;
+        }
+        else if ( tien_ich== null || tien_ich=="") {
+            swal("Tiện ích rỗng", "Vui lòng nhập lại !", "warning");
+            return false;
+        }
+
+
+        else if ( ma_loai== null || ma_loai=="") {
+            swal("Bạn chưa chọn loại căn", "Vui lòng chọn !", "warning");
+            return false;
+        }
+        else if ( ma_quan== null || ma_quan=="") {
+            swal("Bạn chưa chọn quận", "Vui lòng chọn !", "warning");
+            return false;
+        }
+
+        else if ( phuong== null || phuong=="") {
+            swal("Bạn chưa chọn phường", "Vui lòng chọn !", "warning");
+            return false;
+        }
+        
+        
+        else if ( dia_chi== null || dia_chi=="" || dia_chi.length < 10) {
+            swal("Địa chỉ không hợp lệ", "Vui lòng nhập lại !", "warning");
+            return false;
+        }
+        else if ( Number(so_phong_ngu)== "" || Number(so_phong_ngu)<0) {
+            swal("Số phòng ngủ không hợp lệ", "warning");
+            return false;
+        }
+        else if ( Number(so_phong_vs)== "" || Number(so_phong_vs)<0) {
+            swal("Số phòng ngủ không hợp lệ", "warning");
+            return false;
+        }
+        else if ( hinh== null || hinh=="") {
+            swal("Bạn chưa tải hình 1", "Vui lòng tải hình !", "warning");
+            return false;
+        }
+        else if ( hinha== null || hinha=="") {
+            swal("Bạn chưa tải hình 2", "Vui lòng tải hình !", "warning");
+            return false;
+        }
+        else if ( hinhb== null || hinhb=="") {
+            swal("Bạn chưa tải hình 3", "Vui lòng tải hình !", "warning");
+            return false;
+        }
+        else if ( hinhc== null || hinhc=="") {
+            swal("Bạn chưa tải hình 4", "Vui lòng tải hình !", "warning");
+            return false;
+        }
+        
+    }
 </script>
